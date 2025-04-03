e2f034b14bb7fe0463e464812954f0964ef9856b
raghavsar <raghav.sarmukaddam@gmail.com>
2025-04-03 10:38:43 +0530

    Localhost allowed as host, configured for 0.0.0.0

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       port: 5173,
-      host: '192.168.7.154',
+      host: '0.0.0.0',
       // allowedHosts: ['e032-14-97-193-22.ngrok-free.app'],
       proxy: {
         '/api': {
@@ -34,7 +34,6 @@ export default defineConfig(({ mode }) => {
       },
       cors: true,
       hmr: {
-        host: '192.168.7.154'
       }
     },
     define: {
